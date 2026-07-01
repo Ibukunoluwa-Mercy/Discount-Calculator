ba0d2252027e30c838edd8e5fcb1ee3166d4a7e5
Ibukunoluwa Ojedapo <ibukunojedapo2022@gmail.com>
2026-07-01 09:55:26 +0100

    feat: add descriptive discount feedback and update UI styling with new color scheme

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function  calculateDiscount (){
     let amount = document.getElementById("priceInput").value
     let discount = document.getElementById("percentInput").value
+    
     let price = (amount * discount) / 100;
     let percent = amount - price;
 
     document.getElementById("originalAmount").innerHTML = "Original Amount ₦"+  price;
     document.getElementById("discountAmount").innerHTML = "Discount Amount ₦" + percent;
     
+    percentInput.innerHTML= 'Congratulations. You have a discount of' + discount + '%' + 'which means you have ₦' + price + 'chalked off the original amount of ₦' + amount  + '. The new fee you are paying is now ₦' + percent
 }
